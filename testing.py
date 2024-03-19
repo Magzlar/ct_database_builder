df71161312c5a989cea134fdb6a46ff40d201055
Ryan <ryanmaguire1@hotmail.co.uk>
2024-03-19 23:32:17 +0000

    Added some new notes to explain the aims of the project better and added a testing file

---
+++ testing.py
@@ -0,0 +1,13 @@
+import requests
+
+url = r"https://clinicaltrials.gov/api/v2/studies"
+disease_area = 'Psoriatic arthritis'
+fields = "Condition|InterventionType|NumPhases|EnrollmentCount|PrimaryOutcomeMeasure|PrimaryOutcomeTimeFrame|NumPrimaryOutcomes|OrgFullName|StartDate"
+query_term = "AREA[StartDate]RANGE[MIN,2012-01-15]"
+parameters = {'query.cond': disease_area,"fields": fields,"query.term":query_term}
+
+
+response = requests.get(url,params=parameters)
+json = response.json()
+
+print(json["nextPageToken"])
