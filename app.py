08ff66be8c71bb481d400b8765b9464c565c0239
Ryan <ryanmaguire1@hotmail.co.uk>
2024-03-21 20:51:48 +0000

    Removed unused parameter, 'disease area' from the clinical trials class

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ logger = logging.getLogger(__name__)
 def main():
     # set params for API request
     disease_area = 'Psoriatic arthritis'
-    fields = "Condition|InterventionType|NumPhases|EnrollmentCount|PrimaryOutcomeMeasure|PrimaryOutcomeTimeFrame|NumPrimaryOutcomes|OrgFullName|StartDate"
-    query_term = "AREA[StartDate]RANGE[MIN,2012-01-01]"
+    fields = "Condition|InterventionType|NumPhases|EnrollmentCount|PrimaryOutcomeMeasure|PrimaryOutcomeTimeFrame|NumPrimaryOutcomes|OrgFullName|StartDate|PrimaryCompletionDate"
+    query_term = "AREA[StartDate]RANGE[2013-01-01,MAX]"
     parameters = {'query.cond': disease_area, "query.term":query_term, "fields": fields,'format': 'json'}
 
     # make an instance of ClinicalTrial class and fetch the studie
